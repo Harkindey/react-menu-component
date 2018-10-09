b66aae5971a6b92003149061970c35be168ee2f5
Vanessa O <vanneka.okeke@gmail.com>
2018-10-09 21:17:10 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png'
+import searchIcon from './search-icon.png'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <div className="container center">
+        <nav className="menu">
+            <h1 className="menu__logo">Epic Co.</h1>
+
+            <div className="menu__right">
+                <ul className="menu__list">
+                    <li className="menu__list-item"><a className="menu__link menu__link--active" href="#">Home</a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#">About</a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#">Portfolio</a></li>
+                    <li className="menu__list-item"><a className="menu__link" href="#">Contact</a></li>
+                </ul>
+
+                <button className="menu__search-button"></button>
+
+                <form className="menu__search-form hide" method="POST">
+                    <input className="menu__search-input" placeholder="Type and hit enter"/>
+                </form>
+            </div>
+        </nav>
       </div>
     );
   }
