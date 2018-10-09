44f463bf41238f84991815ecdeb9515c9d6921d3
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-10-09 22:26:23 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import logo from './logo.png'
 import searchIcon from './search-icon.png'
 
 class App extends Component {
-  render() {
-    return (
+    render() {
+        return (
 
-        <div className="container center">
-        <nav className="menu">
-            <h1 className="menu__logo">Epic Co.</h1>
+            <div className="container center">
+                <nav className="menu">
+                    <div className="logo__container">
+                        <img src={logo} className="logo" />
+                    </div>
 
-            <div className="menu__right">
-                <ul className="menu__list">
-                    <li className="menu__list-item"><a className="menu__link menu__link--active" href="#">Home</a></li>
-                    <li className="menu__list-item"><a className="menu__link" href="#">About</a></li>
-                    <li className="menu__list-item"><a className="menu__link" href="#">Portfolio</a></li>
-                    <li className="menu__list-item"><a className="menu__link" href="#">Contact</a></li>
-                </ul>
 
-                <button className="menu__search-button"></button>
+                    <div className="menu__right">
+                        <ul className="menu__list">
+                            <li className="menu__list-item"><a className="menu__link menu__link--active" href="#">Home</a></li>
+                            <li className="menu__list-item"><a className="menu__link" href="#">About</a></li>
+                            <li className="menu__list-item"><a className="menu__link" href="#">Portfolio</a></li>
+                            <li className="menu__list-item"><a className="menu__link" href="#">Contact</a></li>
+                        </ul>
 
-                <form className="menu__search-form hide" method="POST">
-                    <input className="menu__search-input" placeholder="Type and hit enter"/>
-                </form>
+                        <button className="menu__search-button"></button>
+
+
+
+                        <form className="menu__search-form hide" method="POST">
+                            <input className="menu__search-input" placeholder="Type and hit enter" />
+                        </form>
+                    </div>
+                </nav>
             </div>
-        </nav>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
